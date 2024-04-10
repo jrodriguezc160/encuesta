6a6602b0bfb5d8cb9a3685a4bd47304096a7703e
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-04-10 13:31:17 +0200

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,16 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div>
-        <a href='' >
+        <a href='../App.js' className='no-bg' >
           {/* <img src='https://www.redurbansol.com/wp-content/uploads/2020/10/diputaciondecaceres.png' alt='' /> */}
           <img src={logo} alt='' />
         </a>
 
-        <a href=''>Encuesta de satisfacción</a>
+        <a href='../App.js'>Encuesta de satisfacción</a>
       </div>
 
       <div>
-        <a href='' >Acceder</a>
+        <a href='../App.js' >Acceder</a>
       </div>
     </div>
   )
